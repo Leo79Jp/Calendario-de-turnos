59b831339950e4abd5eb2dfa06a97a5a7b19f96c
Leo79Jp <leojparodi@gmail.com>
2026-02-12 13:04:53 -0300

    ajuste rutas

--- src/app.js
+++ src/app.js
@@ -7,8 +7,15 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
+
+// --- SOLUCIÓN PARA EL "CANNOT GET /" ---
+// Si alguien entra a la raíz, lo mandamos al calendario
+app.get("/", (req, res) => {
+  res.redirect("/calendario");
+});
+
 app.use("/", require("./routes/turnosRoutes"));
 
-
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}/calendario`));
+// --- SOLUCIÓN PARA EL PUERTO EN RENDER ---
+const PORT = process.env.PORT || 3000; 
+app.listen(PORT, () => console.log(`Servidor en puerto http://localhost:${PORT}`));
